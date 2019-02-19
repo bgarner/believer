77cdae9629507515984de5f329fa473ce02b1221
Megha Jyoti <jyoti5337@gmail.com>
2019-02-18 17:10:28 -0700

    wip

--- index.js
+++ index.js
@@ -1,43 +1,57 @@
+// import {Navigation} from 'react-native-navigation';
+// import {registerScreens} from './src/screens';
+//
+// registerScreens();
+
+// Navigation.events().registerAppLaunchedListener(() => {
+//   Navigation.setDefaultOptions({
+//     topBar: {
+//       noBorder: false,
+//     },
+//     bottomTabs: {
+//       backgroundColor: '#ababab',
+//       translucent: false,
+//       animate: true,
+//     },
+//     bottomTab: {
+//       fontSize: 20,
+//     }
+//   });
+// });
+
+
+// Navigation.events().registerAppLaunchedListener(() => {
+//   Navigation.setRoot({
+//     root: {
+//       stack: {
+//         children: [{
+//           component: {
+//             name: "Home",
+//             options: {
+//               topBar: {
+//                 title: {
+//                   text: 'Home'
+//                 }
+//               }
+//             }
+//           }
+//         }]
+//       }
+//     }
+//   });
+// });
+
 import {Navigation} from 'react-native-navigation';
 import {registerScreens} from './src/screens';
 
 registerScreens();
 
 Navigation.events().registerAppLaunchedListener(() => {
-  Navigation.setDefaultOptions({
-    topBar: {
-      noBorder: false,
-    },
-    bottomTabs: {
-      backgroundColor: '#ababab',
-      translucent: false,
-      animate: true,
-    },
-    bottomTab: {
-      fontSize: 20,
-    }
-  });
-});
-
-Navigation.events().registerAppLaunchedListener(() => {
   Navigation.setRoot({
     root: {
-      stack: {
-        children: [{
-          component: {
-            name: "Home",
-            options: {
-              topBar: {
-                title: {
-                  text: 'Home'
-                }
-              }
-            }
-          }
-        }]
+      component: {
+        name: 'Initializing'
       }
-    }
+    },
   });
 });
-
-
--- src/screens.js
+++ src/screens.js
@@ -1,12 +1,22 @@
 import {Navigation} from 'react-native-navigation';
-import Login from "./components/Login/Login";
-import Home from "./components/Home/Home";
-import ChallengeDetail from "./components/Challenge/ChallengeDetail";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Challenge from "./components/Challenge";
+import Screen2 from "./components/Screen2";
+import Initializing from "./components/Initializing";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
 
 export function registerScreens() {
-  Navigation.registerComponent('Login', () => Login);
+  // Navigation.registerComponent('Login', () => Login);
+
   Navigation.registerComponent('Home', () => Home);
-  Navigation.registerComponent('ChallengeDetail', () => ChallengeDetail);
+  Navigation.registerComponent('Screen2', () => Screen2);
+  Navigation.registerComponent('Challenge', () => Challenge);
+  Navigation.registerComponent('Initializing', () => Initializing);
+  Navigation.registerComponent('SignUp', () => SignUp);
+  Navigation.registerComponent('SignIn', () => SignIn);
+
 
 
 }
